629397d355a7e274b462b5d4924bc48c31ac0b52
Spanyk <paulo.augusto62@hotmail.com>
2023-11-22 10:11:43 -0300

    Adicionando contador

--- src/pages/Aula1/atv1.js
+++ src/pages/Aula1/atv1.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Clock from './relogio';
 import TypingEffect from './letreiro';
+import { Link } from "react-router-dom";
+
 
 export default function Atv1() {
     return (
         <div>
             <TypingEffect />
             <Clock />
+            <Link to={"/"}>Voltar ao Menu</Link>
         </div>
     )
 }
--- src/pages/Aula2/atv2.js
+++ src/pages/Aula2/atv2.js
@@ -1,10 +1,14 @@
 import React from "react"
 import Contador from "./Contador"
 
+import { Link } from "react-router-dom";
+
 export default function Atv2() {
     return (
         <div>
             <Contador />
+            <br />
+            <Link to={"/"}>Voltar ao Menu</Link>
         </div>
     )
 }
